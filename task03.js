9376eb1c49540a74ec81f952e7be8a7fd9056262
rajeshdivya1017 <divyarajesh1007@gmail.com>
2026-02-05 20:59:41 +0530

    Add files via upload

---
+++ task03.js
@@ -0,0 +1,288 @@
+
+let num = 25;
+if (num>10 && num<50)
+{
+    console.log("valid number");
+}
+    else{
+        console.log("invalid number");
+    } 
+
+
+
+
+/* let username = "admin";
+let password = "12345";
+if(username==="admin" && password==="12345")
+{
+    console.log("login successfully");
+}
+else
+{
+    console.log("invalid credentials");
+} */
+
+
+
+    
+    /* let num = 9;
+    num=(num % 2===0)?
+    console.log("even number") : 
+    console.log("odd number"); */
+
+
+
+
+    /* /console.log((10==="10")||(5>2)&&!(3<1));======true(output)
+    explanation..
+       10==="10"          false                                   // in this conditon data type is wrong so its output is false
+       5>2                true                                    // five is greater than two so its output is true
+       3<1                false                                   // tree is greater than one so its output is false
+       !(false)           true                                    // by using NOT operation false becomes true
+       true && true       true                                    // by comparing AND function output is true
+       false || true      true */                                 // by comparing OR function out put is true 
+       
+
+
+
+       /* 
+       console.log("5"+2);  =52                + operator concatenates with num and string
+       console.log("5"-2);  =3                 - operator works with numbers only
+       console.log("5"*2);  =10                * operator works with numbers only 
+       console.log("5"/2);  =2.5               / operator works with numbers only */
+
+
+
+
+/* let value = "100";
+console.log(Number(value));
+console.log(Boolean(value)); */
+
+
+
+
+/* console.log(Boolean(""));      empty strings are always considerd as false
+console.log(Boolean(" "));     space inbetween in the string it may occur some value it it consider as true
+console.log(Boolean(0));       in Boolean 0 is considered as false
+console.log(Boolean([]));      object and arrays are always true even if they are empty */
+
+
+
+/* 
+let marks = 35;
+if(marks>=90)
+{
+    console.log("Grade A");
+}
+else if(marks>=75)
+{
+    console.log("Grade B");
+}
+else if(marks>=50)
+{
+    console.log("Grade C");
+}
+else{
+    console.log("Fail");
+} */
+
+
+
+
+    /* let signal = "yellow";
+switch(signal)
+{
+    case "red":console.log("Stop");
+    break;
+    case "yellow":console.log("Ready");
+    break;
+    case "green":console.log("Go");
+    break;
+    default:console.log("Invalid Signal")
+} */
+
+
+
+
+    /* 
+let age = 18
+let height = 160
+let weight = 55
+if(age>=18)
+{
+    if(height>=160)
+    {
+        if(weight>=50){
+            console.log("Congratulation your are selected");
+        }
+        else
+        {
+            console.log("Your height is not enough");
+        }
+    }
+        else
+        {
+            console.log("Your height is not enough");
+        }
+    }
+        else
+        {
+            console.log("Your age is not enough");
+        } */
+    
+
+
+
+/* for (let i = 1;i<=20;i++)                     In this condition of for loop it check the n num of values untill the condition 
+{                                             will be satisfied..each value of i is printed in every iteration..
+    console.log(i);                           for example: i=1;1<=20;1++  = 2
+}                                                          i=2;2<=20;2++  = 3.... */
+                
+
+
+
+/* for(let i = 1;i<=20;i++)                           the modules operator is used for check odd .
+{                                                  the number which havent have remainder zero means they consider as odd num
+    if(i%2==1)                                      in this condition
+    {
+        console.log(i);
+    }
+} */
+
+
+
+
+    /* let value = 10;                             while loop is run untill the condition is satisfied..
+while(value>=1)                             In this condition it decrese the value by minus of iteration.
+{
+    console.log(value);
+    value--;
+} */
+
+
+
+
+    /* 
+let i = 1;                                      in this do while loop condition ..it excecutes the program utill the condition 
+do                                              satisfied and also  run untill for one false condition occur 
+{
+    console.log(i);
+    i++;
+}
+while(i<=5); */
+
+
+
+
+/* 
+let word = "STACKLY";                           In for loop it is used to iterate over each charcter of the given string
+for(let a of word)
+{
+    console.log(a);
+} */
+
+
+
+
+/* let student = {
+    name : "Arun",
+    course : "MERN",
+    duration : "6 Months"
+};
+for(let b in student)
+{
+    console.log(student[b]);
+} */
+
+
+
+
+/* 
+let user = "admin";
+let pass = "1234";
+let result =(user==="admin" && pass==="1234")?
+"login success":"invalid credentials";
+console.log(result); */
+
+
+
+
+/* let salary = 55000;
+let experiance = 5;
+if(salary>50000 && experiance>3)
+{
+    console.log("Eligible for bonus");
+}
+else
+{
+    console.log("Not Eligible");
+} */
+
+
+
+
+/* let amount =5200;
+if(amount>=5000)
+{
+    console.log("20% Discount");
+}
+else if(amount>=2000) 
+{
+    console.log("10% Discount");
+}
+else
+{
+    console.log("No Discount");
+} */
+
+
+
+
+
+/* let count = 0;
+for(let i = 1; i<=50; i++){
+    if(i % 2 === 0){
+        count++;
+    }
+} 
+    console.log("even numbers count :",count); */
+
+
+
+
+/* let hour = 15;
+if(hour>=1 && hour<=6)
+{
+    console.log("Good Morning");
+}
+else if(hour>=7 && hour<=12)
+{
+    console.log("Morning");
+}
+else if(hour>=13 && hour<=16)
+{
+    console.log("Good Afternoon");
+}
+else if(hour>=17 && hour<=19)
+{
+    console.log("Good Evening");
+}
+else
+{
+    console.log("Good Night");
+} */
+
+
+
+
+
+
+
+
+
+
+       
+       
+       
+
+    
